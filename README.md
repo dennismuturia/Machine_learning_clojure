03fabdfe368c9791d5e6b7d649b86875f6eeeb1a
Dennis Muturia <dmuturia@mo-de.com>
2018-11-27 16:49:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Machine Learning
+
+A Clojure library designed to analyze data and make predictions appropriately
+
+## Usage
+To be updated later
+
+## License
+
+Copyright © 2018 Machine Learning
+
+Distributed under the Dennis :)
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject untitled "0.1.0-SNAPSHOT"
+  :description "This is a site for machine learning"
+  :url "Will fix this soon"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.3.2"]
+                 [compojure "1.6.1"]
+                 [org.postgresql/postgresql "42.2.5"]
+                 [net.mikera/core.matrix "0.20.0"]])
